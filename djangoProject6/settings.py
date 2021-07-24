59a67a1c2cac74534b1ca1b770b151d61b93ab4c
KarthikKashi98 <karthikkashi98@gmail.com>
2021-07-24 18:31:50 +0530

    commit

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -25,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^k1yqrgy(i!%sr95=d&k7(%v8r+7jyc+m=tbj14tv5(_2^nmo#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if (len(sys.argv) >= 2 and sys.argv[1] == 'runserver'):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
